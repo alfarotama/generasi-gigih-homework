24e504024aa188f59a54e44e4a082d4083c2073a
alfarotama <alfarohadiyantama13@gmail.com>
2021-07-13 00:40:42 +0700

    adding home & track file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/home/index";
+import Track from "./pages/home/track";
 
 function App() {
 	console.log(process.env.REACT_APP_SPOTIFY);
 
 	return (
-		<div>
-			<header></header>
+		<div className="App">
+			<Home />
+			<br />
+			<Track />
 		</div>
 	);
 }
---
+++ src/pages/home/track.js
@@ -0,0 +1,19 @@
+const Track = () => {
+	return (
+		<div class="track-info">
+			<img src="" alt="Bohemian Rhapsody" />
+			<p class="title" id="track-title">
+				Track-title
+			</p>
+			<p class="artist" id="artist">
+				Artist
+			</p>
+			<p class="album" id="album">
+				Album
+			</p>
+			<button type="submit">Select</button>
+		</div>
+	);
+};
+
+export default Track;
