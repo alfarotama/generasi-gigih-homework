1d66e18d1a8c2a4988617e7fe8dc941518fb717a
alfarotama <alfarohadiyantama13@gmail.com>
2021-08-03 22:56:07 +0700

    make sure the app and index works

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import "./App.css";
-import Homepage from "./pages/Homepage";
-import CreatePlaylist from "./pages/CreatePlaylist";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
+import './App.css'
+import Homepage from './pages/Homepage'
+import CreatePlaylist from './pages/CreatePlaylist'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function App() {
-	const { isAuthenticated } = useSelector((state) => state.auth);
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Navbar />
-				<main>
-					<Sidebar />
-					<Switch>
-						<Route path="/create-playlist">
-							{isAuthenticated ? <CreatePlaylist /> : <Redirect to="/" />}
-						</Route>
-						<Route path="/">
-							<Homepage />
-						</Route>
-					</Switch>
-				</main>
-			</BrowserRouter>
-		</div>
-	);
+  const { isAuthenticated } = useSelector(state => state.auth)
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <Sidebar />
+          <Switch>
+            <Route path="/create-playlist">
+              {isAuthenticated ? <CreatePlaylist /> : <Redirect to="/" />}
+            </Route>
+            <Route path="/">
+              <Homepage />
+            </Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
